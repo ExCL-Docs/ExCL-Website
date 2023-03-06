3e5cc97b6256c2f126c4bc051d3df658bcd265f3
Aaron Young <ayoung3210@gmail.com>
2023-03-06 12:26:55 -0500

    publish: Make top bar have green text.
    generated from commit 67e561ddc6f3230c1093651e7dd95220cbbcdf6a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "New Systems for 2023",
+        "excerpt":"This is an example post, I haven’t created content yet.  ","categories": ["ExCL"],
+        "tags": ["New Systems"],
+        "url": "/ExCL-Website/excl/new-systems/",
+        "teaser": "/ExCL-Website/assets/images/silicon.jpg"
+      }]
