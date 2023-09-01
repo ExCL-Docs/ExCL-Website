c8be2b894fc89dff05d60947fa5e82455516e25e
Aaron Young <ayoung3210@gmail.com>
2023-09-01 15:06:01 -0400

    publish: Updated site content.
    generated from commit 8241e6aebf2bf10e9786a13cc79b67ac3308e2cb

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,12 +1,12 @@
 var store = [{
         "title": "ExCL July Meeting 2023",
-        "excerpt":"July 2023 ExCL Meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
+        "excerpt":"July 2023 ExCL meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
         "tags": ["User Meetings"],
         "url": "/ExCL-Website/excl/july-meeting-2023/",
         "teaser": "/ExCL-Website/assets/images/july-meeting-2023-splash.png"
       },{
         "title": "ExCL August Meeting 2023",
-        "excerpt":"August 2023 ExCL Meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
+        "excerpt":"August 2023 ExCL meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
         "tags": ["User Meetings"],
         "url": "/ExCL-Website/excl/august-meeting-2023/",
         "teaser": "/ExCL-Website/assets/images/august-meeting-2023-splash.png"
