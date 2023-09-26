5a6459b14d9c28666beff864c4a203095fc82c26
Aaron Young <ayoung3210@gmail.com>
2023-09-26 10:27:06 -0400

    publish: Added March Meeting Slides.
    generated from commit eac254b616da6519206f43086cd517e5393a2f36

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "ExCL March Meeting 2023",
+        "excerpt":"March 2023 ExCL meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
+        "tags": ["User Meetings"],
+        "url": "/excl/march-meeting-2023/",
+        "teaser": "/assets/images/march-meeting-2023-splash.png"
+      },{
         "title": "ExCL July Meeting 2023",
         "excerpt":"July 2023 ExCL meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
         "tags": ["User Meetings"],
