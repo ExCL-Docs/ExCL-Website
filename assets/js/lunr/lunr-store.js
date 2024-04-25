d62bc1146d34442cfb35bf9f952c900e5e9d5a40
Aaron Young <ayoung3210@gmail.com>
2024-04-25 15:59:28 -0400

    publish: Updated slides for April meeting
    generated from commit 6ea79e1756cdcbbdeabed6f4ff6cd05785726ef4

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -28,4 +28,10 @@ var store = [{
         "tags": ["User Meetings"],
         "url": "/excl/january-meeting-2024/",
         "teaser": "/assets/images/january-meeting-2024-splash.png"
+      },{
+        "title": "ExCL April Meeting 2024",
+        "excerpt":"April 2024 ExCL meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
+        "tags": ["User Meetings"],
+        "url": "/excl/april-meeting-2024/",
+        "teaser": "/assets/images/april-meeting-2024-splash.png"
       }]
