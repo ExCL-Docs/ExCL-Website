9bcc5078392bc284a45165ab79193cc638321eac
Aaron Young <ayoung3210@gmail.com>
2024-08-22 13:08:49 -0400

    publish: Uploaded meeting slides.
    generated from commit 306e347e3add51dafc47a6b2059fc92559292ba7

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -46,4 +46,10 @@ var store = [{
         "tags": ["User Meetings"],
         "url": "/excl/july-meeting-2024/",
         "teaser": "/assets/images/july-meeting-2024-splash.jpg"
+      },{
+        "title": "ExCL August Meeting 2024",
+        "excerpt":"August 2024 ExCL meeting slides.   It appears that you don't have a PDF plugin for this browser. You can click here to download the PDF file.   ","categories": ["ExCL"],
+        "tags": ["User Meetings"],
+        "url": "/excl/august-meeting-2024/",
+        "teaser": "/assets/images/august-meeting-2024-splash.png"
       }]
